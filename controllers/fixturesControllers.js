e09256b4cae6ec40df269f6b1e06b4560888df54
Gbolahan Allen <Gbolahanal@HA-SHEM.com>
2019-05-01 21:51:17 +0100

    Add fixturesController.js

---
+++ controllers/fixturesControllers.js
@@ -0,0 +1,58 @@
+const db = require('../models');
+
+module.exports = {
+    createFixture: (req, res) => {
+        const { homeTeamId, awayTeamId } = req.body;
+
+        return db.Fixture.create({ homeTeamId, awayTeamId })
+            .then(fixture => res.status(201).send(fixture))
+            .catch(err => {
+                if (err.name == 'SequelizeForeignKeyConstraintError') {
+                    res.status(400).send('Check team Ids. Only existing teams can be added to fixtures.')
+                    } else {
+                        res.status(500).send('Error creating fixture ' + err.name);
+                    }
+            });
+    },
+
+    getAllFixtures: (req, res) => {
+        return db.Fixture.findAll()
+            .then(fixtures => res.send(fixtures))
+            .catch(err => res.status(404).send(err));
+    },
+
+    getFixtureById: (req, res) => {
+        const id = parseInt(req.params.id);
+
+        return db.Fixture.findByPk(id)
+            .then(fixture => res.send(fixture))
+            .catch(err => res.status(400).send(err));
+    },
+
+    updateFixture: (req, res) => {
+        const id = parseInt(req.params.id);
+        
+        return db.Fixture.findByPk(id)
+            .then(fixture => {
+                const { homeTeamId, awayTeamId } = req.body;
+
+                return fixture.update({ homeTeamId, awayTeamId })
+                    .then(() => res.send(fixture))
+                    .catch(err => res.status(500).send('Error updating fixture.'));
+            })
+            .catch((err) => {
+                res.status(400).send(err)
+            });
+    },
+
+    deleteFixture: (req, res) => {
+        const id = parseInt(req.params.id);
+
+        return db.Fixture.findByPk(id)
+            .then(fixture => fixture.destroy())
+            .then(() => res.send({ id }))
+            .catch((err) => {
+                res.status(400).send(err);
+            });
+    }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -5,13 +5,17 @@ const authMiddleware = require('./authMiddleware');
 const auth = require('../controllers/authController');
 const users = require('../controllers/usersController');
 const teams = require('../controllers/teamsController');
+const fixtures = require('../controllers/fixturesControllers');
 
 //auth
 routes.post('/signup', auth.signUp);
 routes.post('/login', auth.login);
 
-
 routes.use(authMiddleware.verifyUserAuthenticated);
+
+routes.get('/api/fixtures', fixtures.getAllFixtures);
+routes.get('/api/fixtures/:id', fixtures.getFixtureById);
+
 routes.use(authMiddleware.verifyUserIsAdmin);
 
 //users
@@ -19,7 +23,6 @@ routes.get('/api/users', users.getAllUsers);
 routes.get('/api/users/:id', users.getUserById);
 routes.put('/api/users/:id', users.updateUser);
 routes.delete('/api/users/:id', users.deleteUser);
-// routes.post('/api/users', users.createUser);
 
 //teams
 routes.get('/api/teams', teams.getAllTeams);
@@ -27,5 +30,10 @@ routes.get('/api/teams/:id', teams.getTeamById);
 routes.post('/api/teams', teams.createTeam);
 routes.put('/api/teams/:id', teams.updateTeam);
 routes.delete('/api/teams/:id', teams.deleteTeam);
+
+//fixtures
+routes.post('/api/fixtures', fixtures.createFixture);
+routes.put('/api/fixtures/:id', fixtures.updateFixture);
+routes.delete('/api/fixtures/:id', fixtures.deleteFixture);
 
 module.exports = routes;
