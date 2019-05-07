5829c3f6e5824b9c86ea3ceeaea57e42484e0f15
Gbolahan Allen <allengblack@gmail.com>
2019-05-07 23:56:16 +0100

    Add caching to fixtures API

--- src/controllers/fixturesControllers.js
+++ src/controllers/fixturesControllers.js
@@ -1,15 +1,31 @@
 const db = require('../models');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
+const NodeCache = require('node-cache');
+const cache = new NodeCache({ stdTTL: 0, checkPeriod: 0 });
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
 
 module.exports = {
     createFixture: (req, res) => {
         const { homeTeamId, awayTeamId, homeTeamScore, awayTeamScore, matchDate } = req.body;
 
-        const pending = (homeTeamScore == null) && (awayTeamScore == null); //|| this.getDataValue('matchDate') > new Date();
+        const pending = (homeTeamScore == null) && (awayTeamScore == null);
 
         return db.Fixture.create({ homeTeamId, awayTeamId, homeTeamScore, awayTeamScore, matchDate, pending })
-            .then(fixture => res.status(201).send(fixture))
+            .then(fixture => {
+                const key = HOST+PORT+'/api/fixtures/'+fixture.id;
+
+                cache.flushAll();
+                cache.set(key, fixture.toJSON(), (err, success) => {
+                    if(success && !err) {
+                        res.status(201).send(fixture);
+                    } else {
+                        res.status(201).send(fixture)
+                        console.log('caching failed.')
+                    }
+                });
+            })
             .catch(err => {
                 if (err.name == 'SequelizeForeignKeyConstraintError') {
                     res.status(400).send('Check team Ids. Only existing teams can be added to fixtures.')
@@ -20,35 +36,79 @@ module.exports = {
     },
 
     getAllFixtures: (req, res) => {
-        return db.Fixture.findAll({ attributes: ['id', 'homeTeamId', 'awayTeamId', 'homeTeamScore', 'awayTeamScore', 'matchDate'] })
-            .then(fixtures => res.status(200).send(fixtures))
-            .catch(err => res.status(500).send(err));
+        const key = HOST+PORT+'/api/fixtures';
+        const value = cache.get(key); 
+        
+        if (value !== undefined) {
+            console.log('from cache: ' + key)
+            res.status(200).send(value);
+        } else {
+            return db.Fixture.findAll({ 
+                attributes: ['id', 'homeTeamId', 'awayTeamId', 'homeTeamScore', 'awayTeamScore', 'matchDate'], 
+                raw: true 
+            })
+            .then(fixtures => {
+                cache.set(key, fixtures, (err, success) => {
+                    if(success) {
+                        res.status(200).send(fixtures);
+                    } else {
+                        console.log('caching failed');
+                        res.status(200).send(fixtures)
+                    }
+                });
+            })
+            .catch(err => {
+                res.status(500).send(err)
+            });
+        }
     },
 
     getFixtureById: (req, res) => {
         const id = parseInt(req.params.id);
+        const key = HOST+PORT+'/api/fixtures/'+id;
 
-        return db.Fixture.findByPk(id)
-            .then(fixture => {
-                if(fixture == null) res.status(404).send('fixture not found')
-                res.send(fixture)
-            })
-            .catch(err => {
-                res.status(404).send(err)
-            });
+        const value = cache.get(key);
+
+        if(value != undefined) {
+            console.log('from cache: ' + key)
+            res.send(value);
+        } else {
+            return db.Fixture.findByPk(id, { raw: true })
+                .then(fixture => {
+                    if(fixture == null) {
+                        res.status(404).send('fixture not found');
+                    }
+
+                    cache.set(key, fixture, (err, success) => {
+                        if(success) {
+                            res.status(200).send(fixture);
+                        } else {
+                            console.log('caching failed');
+                            res.status(200).send(fixtures)
+                        }
+                    });
+                })
+                .catch(err => {
+                    res.status(500).send(err)
+                });
+        }
     },
 
     updateFixture: (req, res) => {
         const id = parseInt(req.params.id);
+        const key = HOST+PORT+'/api/fixtures/'+id;
         
         return db.Fixture.findByPk(id)
             .then(fixture => {
                 const { homeTeamId, awayTeamId, homeTeamScore, awayTeamScore, matchDate } = req.body;
 
-                const pending = (homeTeamScore == null) && (awayTeamScore == null); //|| this.getDataValue('matchDate') > new Date();
+                const pending = (homeTeamScore == null) && (awayTeamScore == null);
 
                 return fixture.update({ homeTeamId, awayTeamId, homeTeamScore, awayTeamScore, matchDate, pending })
-                    .then(() => res.status(204).send(fixture))
+                    .then(() => {
+                        cache.flushAll();
+                        res.status(204).send(fixture);
+                    })
                     .catch(err => res.status(500).send('Error updating fixture.'));
             })
             .catch((err) => {
@@ -58,11 +118,13 @@ module.exports = {
 
     deleteFixture: (req, res) => {
         const id = parseInt(req.params.id);
+        const key = HOST+PORT+'/api/fixtures/'+id;
 
         return db.Fixture.findByPk(id)
             .then(fixture => {
                 if (fixture == null) res.status(404).send('fixture not found');
-                fixture.destroy()
+                cache.del(key);
+                fixture.destroy();
             })
             .then(() => res.status(204).send({ id }))
             .catch((err) => {
@@ -71,78 +133,98 @@ module.exports = {
     },
 
     searchFixtures: async (req, res) => {
-        let searchParams = { where : {} };
+        const key = req.url;
 
-        const homeTeamName = req.query['homeTeamName'];
-        const awayTeamName = req.query['awayTeamName'];
-        const completed = req.query['completed'];
-        const fixturesBeforeDate = req.query['fixturesBeforeDate'];
-        const fixturesAfterDate = req.query['fixturesAfterDate'];
+        const result = cache.get(key);
 
-        if(homeTeamName !== undefined) {
-            try {
-                let team = await db.Team.findOne({ 
-                    where: { 
-                        name: {
-                            [Op.substring]: homeTeamName
+        if(result != undefined) {
+            console.log('from cache: ' + key)
+            res.send(value);
+        } else {
+            let searchParams = { where : {} };
+
+            const homeTeamName = req.query['homeTeamName'];
+            const awayTeamName = req.query['awayTeamName'];
+            const completed = req.query['completed'];
+            const fixturesBeforeDate = req.query['fixturesBeforeDate'];
+            const fixturesAfterDate = req.query['fixturesAfterDate'];
+
+            if(homeTeamName !== undefined) {
+                try {
+                    let team = await db.Team.findOne({ 
+                        where: { 
+                            name: {
+                                [Op.substring]: homeTeamName
+                            }
                         }
-                    }
-                });
-
-                searchParams.where.homeTeamId = team.id;
-            } catch (error) {
-                res.status(404).send('Home team name provided does not match any team in league')
+                    });
+    
+                    searchParams.where.homeTeamId = team.id;
+                } catch (error) {
+                    res.status(404).send('Home team name provided does not match any team in league')
+                }
             }
-        }
-
-        if(awayTeamName !== undefined) {
-            try {
-                let team = await db.Team.findOne({ 
-                    where: { 
-                        name: {
-                            [Op.substring]: awayTeamName
+    
+            if(awayTeamName !== undefined) {
+                try {
+                    let team = await db.Team.findOne({ 
+                        where: { 
+                            name: {
+                                [Op.substring]: awayTeamName
+                            }
                         }
-                    }
-                });
-                
-                searchParams.where.awayTeamId = team.id;
-            } catch (error) {
-                res.status(404).send('Away team name provided does not match any team in league')
+                    });
+                    
+                    searchParams.where.awayTeamId = team.id;
+                } catch (error) {
+                    res.status(404).send('Away team name provided does not match any team in league')
+                }
             }
-        }
-
-        if(completed !== undefined) {
-            var value = undefined;
-            if (completed == "true") {
-                value = true;
-            } else {
-                value = false;
+    
+            if(completed !== undefined) {
+                var value = undefined;
+                if (completed == "true") {
+                    value = true;
+                } else {
+                    value = false;
+                }
+                searchParams.where.pending = {
+                    [Op.eq] : !value
+                };
             }
-            searchParams.where.pending = {
-                [Op.eq] : !value
-            };
-        }
-
-        if (fixturesAfterDate !== undefined) {
-            if (!searchParams.where.matchDate) {
-                searchParams.where.matchDate = {};
+    
+            if (fixturesAfterDate !== undefined) {
+                if (!searchParams.where.matchDate) {
+                    searchParams.where.matchDate = {};
+                }
+                searchParams.where.matchDate[Op.gte] = fixturesAfterDate;
             }
-            searchParams.where.matchDate[Op.gte] = fixturesAfterDate;
-        }
-
-        if (fixturesBeforeDate !== undefined) {
-            if (!searchParams.where.matchDate) {
-                searchParams.where.matchDate = {};
+    
+            if (fixturesBeforeDate !== undefined) {
+                if (!searchParams.where.matchDate) {
+                    searchParams.where.matchDate = {};
+                }
+                searchParams.where.matchDate[Op.lte] = fixturesBeforeDate;
             }
-            searchParams.where.matchDate[Op.lte] = fixturesBeforeDate;
-        }
+    
 
-        return db.Fixture.findAll({ ...searchParams, attributes: ['id', 'homeTeamId', 'awayTeamId', 'homeTeamScore', 'awayTeamScore', 'matchDate'] })
-            .then((fixtures) => {
-                return res.status(200).send(fixtures);
+            return db.Fixture.findAll({ 
+                ...searchParams, attributes: ['id', 'homeTeamId', 'awayTeamId', 'homeTeamScore', 'awayTeamScore', 'matchDate'],
+                raw: true
             })
-            .catch(err => {
-                res.status(500).send('And error occured ' + err);
-            });
+                .then((fixtures) => {
+                    cache.set(key, fixtures, (err, success) => {
+                        if(success & !err) {
+                            res.status(200).send(fixtures);
+                        } else {
+                            console.log('caching failed');
+                            res.status(200).send(fixtures);
+                        }
+                    });
+                })
+                .catch(err => {
+                    res.status(500).send('And error occured ' + err);
+                });
+            }        
     }
 }
